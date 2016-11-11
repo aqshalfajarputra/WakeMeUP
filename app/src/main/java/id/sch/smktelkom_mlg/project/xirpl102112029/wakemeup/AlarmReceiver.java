ca7fc79c6931f3e72beaf2a6176590ad0ef64c10
Aqshal Fajar Putra <aqshalfajarputra0226@gmail.com>
2016-11-11 07:53:33 +0700

    Initial Commit try

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl102112029/wakemeup/AlarmReceiver.java
@@ -0,0 +1,27 @@
+package id.sch.smktelkom_mlg.project.xirpl102112029.wakemeup;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.widget.Toast;
+
+/**
+ * Created by Aqshal Fajar Putra on 2016/11/11.
+ */
+
+public class AlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Alarm! Wake up! Wake up!", Toast.LENGTH_LONG).show();
+        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        if (alarmUri == null) {
+            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        }
+        Ringtone ringtone = RingtoneManager.getRingtone(context, alarmUri);
+        ringtone.play();
+    }
+}
+
