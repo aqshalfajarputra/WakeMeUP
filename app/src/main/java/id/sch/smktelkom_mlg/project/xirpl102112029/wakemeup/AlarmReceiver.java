284577454b6671e52cb6b4d1fa945f9402b6ae4d
Aqshal Fajar Putra <aqshalfajarputra0226@gmail.com>
2016-11-17 13:33:56 +0700

    Alarm On

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl102112029/wakemeup/AlarmReceiver.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl102112029/wakemeup/AlarmReceiver.java
@@ -3,25 +3,22 @@ package id.sch.smktelkom_mlg.project.xirpl102112029.wakemeup;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.widget.Toast;
+import android.util.Log;
 
 /**
  * Created by Aqshal Fajar Putra on 2016/11/11.
  */
 
 public class AlarmReceiver extends BroadcastReceiver {
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "Alarm! Wake up! Wake up!", Toast.LENGTH_LONG).show();
-        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
-        if (alarmUri == null) {
-            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        }
-        Ringtone ringtone = RingtoneManager.getRingtone(context, alarmUri);
-        ringtone.play();
-    }
-}
 
+    @Override
+    public void onReceive(Context context, Intent arg1) {
+        Log.e("We are in the receiver", "Yay!");
+
+        Intent service_intent = new Intent(context, RingtonePlayingService.class);
+
+        context.startService(service_intent);
+    }
+
+
+}
--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl102112029/wakemeup/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl102112029/wakemeup/MainActivity.java
@@ -2,6 +2,8 @@ package id.sch.smktelkom_mlg.project.xirpl102112029.wakemeup;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -14,13 +16,22 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-    TimePicker alarmTimePicker;
-    PendingIntent pendingIntent;
-    AlarmManager alarmManager;
+    final static int RQS_1 = 1;
+    DatePicker pickerDate;
+    TimePicker pickerTime;
+    Button buttonSetAlarm;
+    Button buttonOffAlarm;
+    TextView info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +40,49 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        alarmTimePicker = (TimePicker) findViewById(R.id.timeJumuk);
-        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        info = (TextView) findViewById(R.id.info);
+        pickerDate = (DatePicker) findViewById(R.id.pickerdate);
+        pickerTime = (TimePicker) findViewById(R.id.pickertime);
+
+        Calendar now = Calendar.getInstance();
+
+        pickerDate.init(
+                now.get(Calendar.YEAR),
+                now.get(Calendar.MONTH),
+                now.get(Calendar.DAY_OF_MONTH),
+                null);
+
+        pickerTime.setCurrentHour(now.get(Calendar.HOUR_OF_DAY));
+        pickerTime.setCurrentMinute(now.get(Calendar.MINUTE));
+
+        buttonSetAlarm = (Button) findViewById(R.id.setalarm);
+        buttonOffAlarm = (Button) findViewById(R.id.buttonOff);
+        buttonSetAlarm.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                Calendar current = Calendar.getInstance();
+
+                Calendar cal = Calendar.getInstance();
+                cal.set(pickerDate.getYear(),
+                        pickerDate.getMonth(),
+                        pickerDate.getDayOfMonth(),
+                        pickerTime.getCurrentHour(),
+                        pickerTime.getCurrentMinute(),
+                        00);
+
+                if (cal.compareTo(current) <= 0) {
+                    //The set Date/Time already passed
+                    Toast.makeText(getApplicationContext(),
+                            "Invalid Date/Time",
+                            Toast.LENGTH_LONG).show();
+                } else {
+
+                    setAlarm(cal);
+                }
+
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -50,6 +102,19 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
+
+    private void setAlarm(Calendar targetCal) {
+
+        info.setText("\n\n***\n"
+                + "Alarm is set@ " + targetCal.getTime() + "\n"
+                + "***\n");
+
+        Intent intent = new Intent(getBaseContext(), AlarmReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getBaseContext(), RQS_1, intent, 0);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, targetCal.getTimeInMillis(), pendingIntent);
+    }
+
 
     @Override
     public void onBackPressed() {
